b95db3dfd980e6216eabe1bdbca187445012fc5c
s-jaswal <525SJASWAL@FRHSD.COM>
2024-10-25 09:47:25 -0400

    Initial Starter Code

---
+++ script.js
@@ -0,0 +1,18 @@
+{
+    "schedule": [
+        {
+            "class": "AP Computer Science",
+            "teacher": "Mr. Kilgore",
+            "room": "203",
+            "period": 1,
+            "days": ["A", "C", "E"]
+        },
+        {
+            "class": "Math Analysis",
+            "teacher": "Ms. Lee",
+            "room": "105",
+            "period": 2,
+            "days": ["B", "D", "F"]
+        }
+    ]
+}
