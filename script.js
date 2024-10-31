2b702ac5a1d0da2850b2977f467da54aa89d347f
s-jaswal <525SJASWAL@FRHSD.COM>
2024-10-31 11:12:27 -0400

    fixed the problem hehe

--- script.js
+++ script.js
@@ -7,13 +7,11 @@ $(document).ready(function () {
     $('#submitDay').on('click', function () {
         const selectedDay = $('#dayInput').val().toUpperCase()
 
-        // Ensure that the input is valid (A-G)
         if (!['A', 'B', 'C', 'D', 'E', 'F', 'G'].includes(selectedDay)) {
             alert('Please enter a valid day (A-G)')
             return
         }
 
-        // Make AJAX request to load the schedule
         $.ajax({
             url: scheduleUrl,
             method: 'GET',
@@ -23,16 +21,13 @@ $(document).ready(function () {
                     item.days.includes(selectedDay)
                 )
 
-                // Clear previous schedule
                 $('#scheduleList').empty()
 
-                // If no classes meet on the selected day, show a message
                 if (daySchedule.length === 0) {
                     $('#scheduleList').append(
                         '<tr><td colspan="5">No classes today.</td></tr>'
                     )
                 } else {
-                    // Display each class in a table row
                     daySchedule.forEach(classItem => {
                         const period = classItem.period
 
